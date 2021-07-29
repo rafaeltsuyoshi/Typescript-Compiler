166c73618a46be2c13237b36207a6f6fe39a28c1
Rafael Tsuyoshi Yamashita <ryamashita@daitan.com>
2021-07-29 07:20:20 -0300

    add unknown-never.ts

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// function printResult(num: number): undefined{
-//     console.log('Result: ' + num)
-//     return
-// }
-// function printResult(num: number): void{
-//     console.log('Result: ' + num)
-//     return
-// }
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// Duvida funciona com os dois ?
-// let combineValues: (a: number, b: number) => void
-var combineValues;
-combineValues = add;
-// combineValues = printResult
-// combineValues = 2
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error ocurred', 500);
+console.log(result);
